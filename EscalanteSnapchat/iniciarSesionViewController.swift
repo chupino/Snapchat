9250508f84184645f0c2dc5f26d28deaab1ffb72
chupino <mauricioescalante903>
2024-05-20 15:20:55 -0500

    google auth

--- EscalanteSnapchat/iniciarSesionViewController.swift
+++ EscalanteSnapchat/iniciarSesionViewController.swift
@@ -8,12 +8,34 @@
 import UIKit
 import Firebase
 import FirebaseAuth
+import GoogleSignIn
 
 class iniciarSesionViewController: UIViewController {
 
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBAction func iniciarSesionTapped(_ sender: Any) {
+        print("Intentando iniciar Sesion")
+        guard let clientID = FirebaseApp.app()?.options.clientID else { return }
+        let config = GIDConfiguration(clientID: clientID)
+        GIDSignIn.sharedInstance.configuration = config
+        GIDSignIn.sharedInstance.signIn(withPresenting: self) { [unowned self] result, error in
+          guard error == nil else {
+              print("Se produjo un error \(error)")
+            return
+          }
+
+          guard let user = result?.user,
+            let idToken = user.idToken?.tokenString
+          else {
+              
+            return
+          }
+
+          let credential = GoogleAuthProvider.credential(withIDToken: idToken,
+                                                         accessToken: user.accessToken.tokenString)
+            print("Sesion iniciada")
+            /*
         Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!) {
             (user,error) in
             print("Intentando iniciar sesión")
@@ -21,7 +43,7 @@ class iniciarSesionViewController: UIViewController {
                 print("Se presento el siguiente error: \(error)")
             }else{
                 print("Inicio de sesión exitoso")
-            }
+            }*/
         }
     }
     override func viewDidLoad() {
